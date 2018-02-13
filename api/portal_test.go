051c96eb20db4d49be63ec085aedba4926631833
toros11 <tobias.rosen@axsh.net>
2018-02-13 22:45:02 +0900

    Updated test

--- api/portal_test.go
+++ api/portal_test.go
@@ -1,16 +1,25 @@
 package api
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
 )
 
 func TestNewPortalClient(t *testing.T) {
 	assert := assert.New(t)
 
-	p := NewPortalClient("0.0.0.0", "8002")
+	p, ctx := NewPortalClient("0.0.0.0", "8002", true, "cert-file", "token",)
+
 	assert.NotNil(p)
 	assert.Equal("0.0.0.0", p.hostIp)
 	assert.Equal("8002", p.hostPort)
+	assert.Equal(true, p.insecure)
+	assert.Equal("cert-file", p.certFile)
+
+	md, ok := metadata.FromOutgoingContext(ctx)
+	assert.True(ok)
+	assert.Equal("token", strings.Join(md["auth_token"], ""))
 }
